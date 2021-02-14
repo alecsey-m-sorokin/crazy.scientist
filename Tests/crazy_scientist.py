03d087152d7f04f4ce00032624cea489640b316d
Administrator <ams@tut.by>
2021-02-14 21:50:04 +0300

    changes 14/02/2021

--- Tests/crazy_scientist.py
+++ Tests/crazy_scientist.py
@@ -32,10 +32,9 @@ def AuthorizationGame(Hash, regToken):
                                                params={'Hash': Hash, 'Token': regToken, 'MobilePlatform': 'false'},
                                                json=params_AuthorizationGame)
     response = response_AuthorizationGame.json()
+    url = response_AuthorizationGame.url
     assert response_AuthorizationGame.status_code == 200
     return response
-
-
 """ -------------------------------------------------------------------------------------------- """
 
 """ GetSlotInfo -------------------------------------------------------------------------------- """
@@ -51,8 +50,6 @@ def GetSlotInfo(Hash, regToken):
     response = response_GetSlotInfo.json()
     assert response_GetSlotInfo.status_code == 200
     return response
-
-
 """ -------------------------------------------------------------------------------------------- """
 
 """ CreditDebit -------------------------------------------------------------------------------- """
@@ -72,60 +69,134 @@ def CreditDebit(Hash, regToken):
     assert response_CreditDebit.status_code == 200
     # CreditDebit_TokenAsync = response['TokenAsync']
     return response
-
-
 """ -------------------------------------------------------------------------------------------- """
 
-# xxx_CreditDebit = CreditDebit(hash_CreditDebit, regToken_crazy_scientist)
-# TokenAsync = xxx_CreditDebit["TokenAsync"]
-# print('TokenAsync_CreditDebit = ', TokenAsync)
-# time.sleep(1)
-
-
 """ GetAsyncResponse --------------------------------------------------------------------------- """
-
-
 # @pytest.mark.parametrize(regToken_crazy_scientist, TokenAsync)
 def GetAsyncResponse(regToken, TA):
     hash_GetAsyncResponse = hashlib.md5(('GetAsyncResponse/' + TA + A.gameKey).encode('utf-8')).hexdigest()
     print('hash_GetAsyncResponse = ', hash_GetAsyncResponse)
     params_GetAsyncResponse = {'Hash': hash_GetAsyncResponse, 'Token': regToken_crazy_scientist, 'TokenAsync': TA}
-
-    response_GetAsyncResponse = requests.post(D.DOMAIN + '/games/GetAsyncResponse',
-                                              params={'Hash': hash_GetAsyncResponse, 'Token': regToken,
-                                                      'TokenAsync': TA}, json=params_GetAsyncResponse)
-
+    response_GetAsyncResponse = requests.post(D.DOMAIN + '/games/GetAsyncResponse', params={'Hash': hash_GetAsyncResponse, 'Token': regToken, 'TokenAsync': TA}, json=params_GetAsyncResponse)
     response = response_GetAsyncResponse.json()
     # print('url =', response_GetAsyncResponse.url)
+    url = response_GetAsyncResponse.url
     assert response_GetAsyncResponse.status_code == 200
     # GetAsyncResponse_ResultId = response['ResultId']
     return response
-
-
 """ -------------------------------------------------------------------------------------------- """
 
-xxx_AuthorizationGame = AuthorizationGame(hash_AuthorizationGame, regToken_crazy_scientist)
-xxx_GetSlotInfo = GetSlotInfo(hash_GetSlotInfo, regToken_crazy_scientist)
-xxx_CreditDebit = CreditDebit(hash_CreditDebit, regToken_crazy_scientist)
-TokenAsync = xxx_CreditDebit["TokenAsync"]
-print('TokenAsync_CreditDebit = ', TokenAsync)
-time.sleep(1)
+""" DiceBonusGame ------------------------------------------------------------------------------ """
+def hash_DiceBonusGame(RT, ResultId, SpinId, BonusGameID):
+    HASH = hashlib.md5(('DiceBonusGame/' + RT + ResultId + SpinId + BonusGameID + A.gameKey).encode('utf-8')).hexdigest()
+    # var tmp2 = 'DiceBonusGame/' + pm.environment.get("token_crazy") + pm.environment.get("resultId") + pm.environment.get("spinId") + pm.environment.get("BonusGameId") + gameKey
+    print('hash_DiceBonusGame = ', HASH)
+    return HASH
 
 
-xxx_GetAsyncResponse = GetAsyncResponse(regToken_crazy_scientist, TokenAsync)
-print(xxx_GetAsyncResponse)
-GetAsyncResponse_ResultId = xxx_GetAsyncResponse['ResultId']
-print('GetAsyncResponse_ResultId = ', GetAsyncResponse_ResultId)
+# @pytest.mark.parametrize(hash_DiceBonusGame, regToken_crazy_scientist, resultId, BonusGameId, SpinId, Info)
+def DiceBonusGame(Hash, regToken, rId, bgId, sId, info):
+    params_DiceBonusGame = {"Hash": Hash, "Token": regToken, "ResultId": rId, "BonusGameId": bgId, "SpinId": sId, "Info": info}
+    response_DiceBonusGame = requests.post(D.DOMAIN + '/bonus/DiceBonusGame', params={"Hash": Hash, "Token": regToken, "ResultId": rId, "BonusGameId": bgId, "SpinId": sId, "Info": info}, json=params_DiceBonusGame)
+    response = response_DiceBonusGame.json()
+    assert response_DiceBonusGame.status_code == 200
+    url = response_DiceBonusGame.url
+    print(url)
+    # DiceBonusGame_TokenAsync = response['TokenAsync']
+    return response
+""" -------------------------------------------------------------------------------------------- """
 
-BetSum = 'Bet Sum = ' + str(xxx_GetAsyncResponse["BetSum"])
-WinSum = 'Win Sum = ' + str(xxx_GetAsyncResponse["WinInfo"]["WinSum"])
-Credit_before_spin = 'CREDIT before spin = ' + str(xxx_GetAsyncResponse["WinInfo"]["Balance"] - xxx_GetAsyncResponse["WinInfo"]["WinSum"] + xxx_GetAsyncResponse["BetSum"])
-Credit_after_spin = 'CREDIT after spin  = ' + str(xxx_GetAsyncResponse["WinInfo"]["Balance"])
-FreeSpinsCount = 'FreeSpinCount = ' + str(xxx_GetAsyncResponse["FreeSpinsCount"])
-CurrentSpinWin = 'CurrentSpinWin = ' + str(xxx_GetAsyncResponse["WinInfo"]["CurrentSpinWin"])
-BonusGameId = 'BonusGameId = ' + str(xxx_GetAsyncResponse["SpinResult"]["WinCoins"])
-print('\n', BetSum, '\n', WinSum, '\n', Credit_before_spin, '\n', Credit_after_spin, '\n', FreeSpinsCount, '\n', CurrentSpinWin)
+""" GetAsyncResponse_DiceBonusGame ------------------------------------------------------------- """
+# @pytest.mark.parametrize(regToken_crazy_scientist, TokenAsync)
+def GetAsyncResponse_DiceBonusGame(regToken, TokenAsyncDice):
+    hash_GetAsyncResponse_DiceBonusGame = hashlib.md5(('GetAsyncResponse/' + TokenAsyncDice + A.gameKey).encode('utf-8')).hexdigest()
+    print('hash_GetAsyncResponse = ', hash_GetAsyncResponse_DiceBonusGame)
+    params_GetAsyncResponse_DiceBonusGame = {'Hash': hash_GetAsyncResponse_DiceBonusGame, 'Token': regToken_crazy_scientist, 'TokenAsync': TokenAsyncDice}
+    response_GetAsyncResponse_DiceBonusGame = requests.post(D.DOMAIN + '/games/GetAsyncResponse', params={'Hash': hash_GetAsyncResponse_DiceBonusGame, 'Token': regToken, 'TokenAsync': TokenAsyncDice}, json=params_GetAsyncResponse_DiceBonusGame)
+    response = response_GetAsyncResponse_DiceBonusGame.json()
+    assert response_GetAsyncResponse_DiceBonusGame.status_code == 200
+    # GetAsyncResponse_ResultId = response['ResultId']
+    return response
+""" -------------------------------------------------------------------------------------------- """
 
+
+xxx_AuthorizationGame = AuthorizationGame(hash_AuthorizationGame, regToken_crazy_scientist)  # авторизация в игре по токену ! AuthorizationGame
+xxx_GetSlotInfo = GetSlotInfo(hash_GetSlotInfo, regToken_crazy_scientist)  # получение информации о игре ! GetSlotInfo
+
+i = 1
+while i < 50:
+    while True:
+        xxx_CreditDebit = CreditDebit(hash_CreditDebit, regToken_crazy_scientist)  # ставка ! CreditDebit
+        TokenAsync = xxx_CreditDebit["TokenAsync"]
+        print('TokenAsync_CreditDebit = ', TokenAsync)
+        time.sleep(1)
+
+        xxx_GetAsyncResponse = GetAsyncResponse(regToken_crazy_scientist, TokenAsync)  # асинхронный ответ ! GetAsyncResponse
+        GetAsyncResponse_ResultId = xxx_GetAsyncResponse['ResultId']
+        print('GetAsyncResponse_ResultId = ', GetAsyncResponse_ResultId)
+        if xxx_GetAsyncResponse["SpinResult"]["DiceGame"] is None:
+            BonusGameId = "no bonus game"
+            print("BonusGameId =", BonusGameId)
+            continue
+        else:
+            BonusGameId = xxx_GetAsyncResponse["SpinResult"]["DiceGame"]["Id"]
+            print("BonusGameId =", BonusGameId)
+            Info = 'true'
+            spinId = xxx_GetAsyncResponse["SpinResult"]["Id"]
+            print("SpinId =", spinId)
+            xxx_hash_DiceBonusGame = hash_DiceBonusGame(regToken_crazy_scientist, GetAsyncResponse_ResultId, spinId, BonusGameId)
+            xxx_DiceBonusGame = DiceBonusGame(xxx_hash_DiceBonusGame, regToken_crazy_scientist, GetAsyncResponse_ResultId, BonusGameId, spinId, Info)  # # кидаем кубик в бонусной игре ! DiceBonusGame
+            DiceBonusGame_TokenAsync = xxx_DiceBonusGame["TokenAsync"]
+            print('TokenAsync_DiceBonusGame = ', DiceBonusGame_TokenAsync)
+            time.sleep(1)
+
+            xxx_GetAsyncResponse_DiceBonusGame = GetAsyncResponse_DiceBonusGame(regToken_crazy_scientist, DiceBonusGame_TokenAsync)  # асинхронный ответ в бонусной игре ! GetAsyncResponse
+            Info = 'false'
+            ThrowsLeft = xxx_GetAsyncResponse_DiceBonusGame["ThrowsLeft"]
+            print("ThrowsLeft =", ThrowsLeft)
+            print(xxx_GetAsyncResponse_DiceBonusGame)
+            while ThrowsLeft > 0:
+                xxx_DiceBonusGame = DiceBonusGame(xxx_hash_DiceBonusGame, regToken_crazy_scientist, GetAsyncResponse_ResultId, BonusGameId, spinId, Info)  # # кидаем кубик в бонусной игре ! DiceBonusGame
+                DiceBonusGame_TokenAsync = xxx_DiceBonusGame["TokenAsync"]
+                time.sleep(1)
+                xxx_GetAsyncResponse_DiceBonusGame = GetAsyncResponse_DiceBonusGame(regToken_crazy_scientist, DiceBonusGame_TokenAsync)  # асинхронный ответ в бонусной игре ! GetAsyncResponse
+                ThrowsLeft = xxx_GetAsyncResponse_DiceBonusGame["ThrowsLeft"]
+                print("ThrowsLeft =", ThrowsLeft)
+                print(xxx_GetAsyncResponse_DiceBonusGame)
+                WinType = xxx_GetAsyncResponse_DiceBonusGame["SelectedSector"]["WinType"]
+                print("WinType =", WinType)
+                if WinType == 7:
+                    print('! BONUS CARD GAME !')
+                    break
+            break
+    print('i = ', i)
+    i = i + 1
+
+
+
+
+
+
+    # BetSum = 'Bet Sum = ' + str(xxx_GetAsyncResponse["BetSum"])
+    # WinSum = 'Win Sum = ' + str(xxx_GetAsyncResponse["WinInfo"]["WinSum"])
+    # Credit_before_spin = 'CREDIT before spin = ' + str(xxx_GetAsyncResponse["WinInfo"]["Balance"] - xxx_GetAsyncResponse["WinInfo"]["WinSum"] + xxx_GetAsyncResponse["BetSum"])
+    # Credit_after_spin = 'CREDIT after spin  = ' + str(xxx_GetAsyncResponse["WinInfo"]["Balance"])
+    # FreeSpinsCount = 'FreeSpinCount = ' + str(xxx_GetAsyncResponse["FreeSpinsCount"])
+    # CurrentSpinWin = 'CurrentSpinWin = ' + str(xxx_GetAsyncResponse["WinInfo"]["CurrentSpinWin"])
+    # BonusGameId = 'BonusGameId = ' + str(xxx_GetAsyncResponse["SpinResult"]["DiceGame"])
+    # if xxx_GetAsyncResponse["SpinResult"]["DiceGame"] is None:
+
+    #     BonusGameId = "no bonus game"
+    #     print("BonusGameId =", BonusGameId)
+    # else:
+    #     BonusGameId = xxx_GetAsyncResponse["SpinResult"]["DiceGame"]["Id"]
+    #     print("BonusGameId =", BonusGameId)
+    #     Info = 'true'
+    #     spinId = xxx_GetAsyncResponse["SpinResult"]["Id"]
+    #     print("SpinId =", spinId)
+    #
+    # print('\n', BetSum, '\n', WinSum, '\n', Credit_before_spin, '\n', Credit_after_spin, '\n', FreeSpinsCount, '\n',
+    #       CurrentSpinWin, '\n', BonusGameId)
 
 if __name__ == "__main__":
     unittest.main()
